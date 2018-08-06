87012b6ab9b0b388cf4398937ca5115e98a08934
bbergan <bernard@berganmail.com>
2018-08-05 20:37:13 -0500

    add router logic

--- src/routes/urlRouter.ts
+++ src/routes/urlRouter.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import * as urlController from '../controllers/url';
+import * as urlController from '../controllers/urlController';
 
 export class UrlRouter {
     router: Router
